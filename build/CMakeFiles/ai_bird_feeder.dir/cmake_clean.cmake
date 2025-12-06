fa2687404597f737a451a1cc94d0309367408d04
Lior Roitman <liorroitman1@gmail.com>
2025-12-06 22:05:07 +0000

    First files

---
+++ build/CMakeFiles/ai_bird_feeder.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ai_bird_feeder.dir/src/EventDetector.cpp.o"
+  "CMakeFiles/ai_bird_feeder.dir/src/EventDetector.cpp.o.d"
+  "CMakeFiles/ai_bird_feeder.dir/src/main.cpp.o"
+  "CMakeFiles/ai_bird_feeder.dir/src/main.cpp.o.d"
+  "ai_bird_feeder"
+  "ai_bird_feeder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ai_bird_feeder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ai_bird_feeder.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ai_bird_feeder.
+# This may be replaced when dependencies are built.
